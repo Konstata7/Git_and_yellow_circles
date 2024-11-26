6d5b3b66b2cf349054859c173d18a6d282d73ab8
Konstantin <konstantinkarasenko@gmail.com>
2024-11-26 15:33:19 +0300

    Second Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6 import uic
+from PyQt6 import uic, QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
@@ -9,9 +9,21 @@ class GitAndYellowCircles(QMainWindow):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.button_clicked)
+        self.drawn = False
 
     def button_clicked(self):
-        pass
+        from random import randrange
+        canvas = self.label.pixmap()
+        painter = QtGui.QPainter(canvas)
+        pen = QtGui.QPen()
+        pen.setWidth(1)
+        pen.setColor(QtGui.QColor(255, 247, 0))
+        painter.setPen(pen)
+
+        painter.drawEllipse(21, 101, randrange(22, 780), randrange(102, 681))
+        painter.end()
+        self.drawn = True
+        self.label.setPixmap(canvas)
 
 
 if __name__ == '__main__':
